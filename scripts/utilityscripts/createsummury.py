ebc3c6c5f15b0ad95e819184e6889b656af6533c
Archishmaan Peyyety <archishou@gmail.com>
2020-06-06 17:10:00 -0400

    sum
    Signed-off-by: Archishmaan Peyyety <archishou@gmail.com>

--- scripts/utilityscripts/createsummury.py
+++ scripts/utilityscripts/createsummury.py
@@ -7,15 +7,21 @@ summury = [[0, "a", "b", "c"]]
 with open(data_set_path, 'r') as data_set:
     large = [line for line in csv.reader(data_set, delimiter=',')]
 
+found = False
+
 for num, large_row in enumerate(large, start=0):
     for index, item in enumerate(summury, start=0):
         if large_row[2] == item[1] and large_row[3] == item[2] and large_row[4] == item[3] and large_row[5] == item[4]:
             summury[index][0] = summury[index][0] + 1
+            found = True
             break
-    summury.append([1, large_row[2], large_row[3], large_row[4], large_row[5]])
+    if not found:
+        summury.append([1, large_row[2], large_row[3], large_row[4], large_row[5]])
 
 print(summury)
 
 with open(summury_path, 'w+') as file:
     csvWriter = csv.writer(file, delimiter=',')
     csvWriter.writerows(summury)
+
+print(len(summury))
